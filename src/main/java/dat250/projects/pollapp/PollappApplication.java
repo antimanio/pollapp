bd0199b81b552e1bf57468cefa8580e75a6e9244
BG <biljan.georgiev@hotmail.com>
2025-10-15 10:58:18 +0200

    test biljan

--- src/main/java/dat250/projects/pollapp/PollappApplication.java
+++ src/main/java/dat250/projects/pollapp/PollappApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PollappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PollappApplication.class, args);
+		SpringApplication.run(PollappApplication.class, args);   // test
+		
+		
 	}
 
 }
