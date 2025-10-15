2d6c3ce95782b5de7834e6c4b3ed381839840d9f
BG <biljan.georgiev@hotmail.com>
2025-10-15 11:33:54 +0200

    Struktur, usikker om pakkenavn

--- src/main/java/dat250/projects/pollapp/PollappApplication.java
+++ src/main/java/dat250/projects/pollapp/PollappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PollappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PollappApplication.class, args);   // test
+		SpringApplication.run(PollappApplication.class, args);   
 		
 		
 	}
